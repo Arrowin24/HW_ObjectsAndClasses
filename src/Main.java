9724f1b270faf9b4c6ad800b8241d77ee7bc99f5
Arrowin24 <igor_rudenko.ru@mail.ru>
2022-09-26 22:46:16 +0700

    HW_Solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author ruAuthor = new Author("Dmitriy", "Dunaev");
+        Author enAuthor = new Author("James", "Johns");
+        Book ruBook = new Book("Astronavt",1985,ruAuthor);
+        Book enBook = new Book("Deep",1998, enAuthor);
+        printBook(ruBook);
+        ruBook.setYear(2005);
+        System.out.println("New book year: "+ruBook.getYear());
+
+    }
+
+    public static void printBook(Book book){
+        System.out.println("Book: "+book.getName());
+        System.out.println("Author: "+book.getAuthor().getSurname()+" "+book.getAuthor().getName());
+        System.out.println("Publication year: "+book.getYear());
     }
 }
